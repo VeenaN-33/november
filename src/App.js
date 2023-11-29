d47df8963acebe636a966478ee822304656a5817
veena8867 <148980366+veena8867@users.noreply.github.com>
2023-11-29 12:32:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navigation from './Components/Navigation'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home1 from './Components/Home1'
+import Contact from './Components/Contact'
+import  WeatherApp from  './Components/WeatherApp/WeatherApp'
+import ReactPlayer from 'react-player'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+      <Navigation/>
     </div>
-  );
+    <Routes>
+      <Route path='/' element={<Home1/>}/>
+      <Route path='/contact'element={<Contact/>}/>
+      <Route path='/weather' element={<WeatherApp/>}/>
+
+    </Routes>
+    </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
